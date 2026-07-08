c58adeb5358d49359b295206d5916f32e2e4c4d4
BharathM1717 <24ece.bharath.m@skct.edu.in>
2026-07-08 12:20:04 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,50 @@
+// console.log("hello");
+// let a=10
+// let b=4
+// console.log(a+b);//identifier
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// //relational
+// let age=20
+// console.log(age<18);
+// console.log(age>18);
+// console.log(age==18);
+// console.log(age!=18);
+// //logical
+// let voterid=true
+// console.log(age>=18 && voterid==true);
+// console.log(age<=18 || voterid==true);
+// console.log(!voterid);
+// //conditional statement
+//     //if statement
+// let ticket=false
+// if(ticket==true){//block
+//     console.log("allow");
+// }
+// else{
+//     console.log("not allowed");
+// }
+// //switch case
+// let dept="ece"
+// switch(dept){
+//     case "ece":
+//         console.log("c6");
+//         break;
+//     case "cse":
+//         console.log("c3");
+//         break;
+//     case "it":
+//         console.log("c7")
+//     default:
+//         console.log("enter valid dept")
+// }
+// let num1=10;
+// let num2=12
+// let value=true;
+// console.log(!value);
+let age = 10;
+let temp = 13;
+console.log(age>=18 && temp===101)
+console.log(age>=18||temp===20)
